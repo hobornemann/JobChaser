b19672b3b0541f289c5f421684644865ef9f8d7d
Hans-Olov <hobornemann@gmail.com>
2024-03-11 19:14:57 +0100

    added conditional rendering in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,45 @@ import { getJobs } from './jobs';
 function App() {
 
   const jobs = getJobs()
-
-  return (
-    <div className='jobChaserContainer'>
+/*   const jobs = "" */
+  if(jobs){
+    return (
+      <div className='jobChaserContainer'>
+        <Header />
+        <main>        
+          {jobs.map((job) => {
+            return (
+              <JobCard
+                key={job.id}
+                company={job.company}
+                logoUrl={job.logo}
+                position={job.position}
+                role={job.role}
+                level={job.level}
+                postedAt={job.postedAt}
+                contract={job.contract}
+                location={job.location}
+                languages={job.languages}
+                tools={job.tools}
+              />
+            );
+          })}
+        </main>
+        <Footer />
+      </div>
+    );
+  } else {
+    return(
+      <div className='jobChaserContainer'>
       <Header />
-      <main>
-        {jobs.map((job) => {
-          return (
-            <JobCard
-              key={job.id}
-              company={job.company}
-              logoUrl={job.logo}
-              position={job.position}
-              role={job.role}
-              level={job.level}
-              postedAt={job.postedAt}
-              contract={job.contract}
-              location={job.location}
-              languages={job.languages}
-              tools={job.tools}
-            />
-          );
-        })}
+      <main>        
+        <p>The jobs list cannot be loaded. Please try again later.</p>
       </main>
       <Footer />
     </div>
-  );
+    )
+    
+  }
 }
 
 export default App
