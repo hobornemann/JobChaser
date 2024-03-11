1115fb802c782bacf5bd62c5609676bc04bb0973
Hans-Olov <hobornemann@gmail.com>
2024-03-11 14:44:12 +0100

    SearchBar.jsx and .css

---
+++ src/SearchBar.jsx
@@ -0,0 +1,17 @@
+import './styles/searchBar.css'
+
+export default SearchBar
+
+
+function SearchBar(){
+    <div className="searchBar">
+        <div className="search-icon-and-input">
+            <img src="" alt="" className="search-icon" />
+            <input type="text" className="search-input" placeholder="Write your search text here"/>
+        </div>
+        <div className="buttons">
+            <button>Tag 1</button>
+            <button>Clear</button>
+        </div>
+    </div>
+}
